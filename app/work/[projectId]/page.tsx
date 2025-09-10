7d4e0013842a6bfbe88281df139bb0f78f608f2d
Austin Knobloch <austin.knobloch@ledgestone.com>
2025-09-09 20:42:38 -0500

    Add generateStaticParams for dynamic project routes
    - Add generateStaticParams function to /work/[projectId] page
    - Pre-generate static pages for all project IDs at build time
    - Fix static export compatibility for dynamic routes

--- app/work/[projectId]/page.tsx
+++ app/work/[projectId]/page.tsx
@@ -1,9 +1,16 @@
 import ProjectDetail from '../../components/ProjectDetail'
+import { projects } from '../../data/projects'
 
 interface ProjectPageProps {
   params: {
     projectId: string
   }
+}
+
+export async function generateStaticParams() {
+  return projects.map((project) => ({
+    projectId: project.id,
+  }))
 }
 
 export default function ProjectPage({ params }: ProjectPageProps) {
